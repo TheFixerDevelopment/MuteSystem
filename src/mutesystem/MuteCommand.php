882ba777f54fd78ad4cb96c5a0231130138b23ea
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2017-12-13 12:43:48 +0000

    Fixed.

--- src/mutesystem/MuteCommand.php
+++ src/mutesystem/MuteCommand.php
@@ -2,6 +2,8 @@
 
 namespace mutesystem;
 
+use mutesystem\Manager;
+use mutesystem\translation\Translation;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat;
