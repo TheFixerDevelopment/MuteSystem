3fb700cc0e16d744f374f08ecc7a85cf64aba513
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2017-12-13 11:59:55 +0000

    Update Translation.php

--- src/mutesystem/translation/Translation.php
+++ src/mutesystem/translation/Translation.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace bansystem\translation;
+namespace mutesystem\translation;
 
-use bansystem\exception\TranslationFailedException;
+use mutesystem\exception\TranslationFailedException;
 use InvalidArgumentException;
 use pocketmine\command\Command;
 use pocketmine\utils\TextFormat;
@@ -15,30 +15,14 @@ class Translation {
                 return TextFormat::RED . "Not showing command information due to self-leak issues.";
             case "playerNotFound":
                 return TextFormat::GOLD . "Player is not online.";
-            case "playerAlreadyBanned":
-                return TextFormat::GOLD . "Player is already banned.";
-            case "ipAlreadyBanned":
-                return TextFormat::GOLD . "Player is already IP banned.";
-            case "ipNotBanned":
-                return TextFormat::GOLD . "IP address is not banned.";
             case "ipAlreadyMuted":
                 return TextFormat::GOLD . "IP address is already muted.";
-            case "playerNotBanned":
-                return TextFormat::GOLD . "Player is not banned.";
             case "playerAlreadyMuted":
                 return TextFormat::GOLD . "Player is already muted.";
             case "playerNotMuted":
                 return TextFormat::GOLD . "Player is not muted.";
             case "ipNotMuted":
                 return TextFormat::GOLD . "IP address is not muted.";
-            case "playerAlreadyBlocked":
-                return TextFormat::GOLD . "Player is already blocked.";
-            case "playerNotBlocked":
-                return TextFormat::GOLD . "Player is not blocked.";
-            case "ipAlreadyBlocked":
-                return TextFormat::GOLD . "IP address is already blocked.";
-            case "ipNotBlocked":
-                return TextFormat::GOLD . "IP address is not blocked.";
             default:
                 throw new TranslationFailedException("Failed to translate.");
         }
